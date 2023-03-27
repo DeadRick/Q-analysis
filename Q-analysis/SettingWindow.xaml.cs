51a9781cb199f1f74a3c78576a7087084c6f4aa9
Victor <39325834+DeadRick@users.noreply.github.com>
2023-03-28 02:22:45 +0300

    add files

--- Q-analysis/SettingWindow.xaml.cs
+++ Q-analysis/SettingWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace Q_analysis
         private void InitializeSettingWindow()
         {
             InitializeComponent();
+            //one
             dataGrid.Visibility = Visibility.Hidden;
             this.MatrixSize = Enumerable.Range(1, 20).ToArray();
             this.DataContext = this;
